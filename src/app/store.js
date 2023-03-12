9bf626f870241f1d9826dbb20a6018ed83717102
Muhammad SA <MuhammadSA211999>
2023-03-12 19:40:13 +0600

    initial setup

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,10 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import { apiSlice } from "../features/api/apiSlice";
+import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "../features/api/apiSlice";
 
-// export const store = configureStore({
-//     reducer: {
-//         [apiSlice.reducerPath]: apiSlice.reducer,
-//     },
-//     middleware: (getDefaultMiddlewares) =>
-//         getDefaultMiddlewares().concat(apiSlice.middleware),
-// });
+export const store = configureStore({
+    reducer: {
+        [apiSlice.reducerPath]: apiSlice.reducer,
+    },
+    middleware: (getDefaultMiddlewares) =>
+        getDefaultMiddlewares().concat(apiSlice.middleware),
+});
